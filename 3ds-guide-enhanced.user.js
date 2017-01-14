02d05cd49d18fa70576644ef508f3f5c84c6b7bb
IvanFon <ivanfonseca55@gmail.com>
2017-01-14 01:58:54 -0500

    Bumped up version.

--- 3ds-guide-enhanced.user.js
+++ 3ds-guide-enhanced.user.js
@@ -26,7 +26,7 @@
 // ==UserScript==
 // @name         3DS Guide Enhanced
 // @namespace    https://ivanfon.github.io/
-// @version      0.2.1
+// @version      0.2.2
 // @description  A browser extension with enhancements for the 3DS hacking guide found at https://3ds.guide/
 // @author       Ivan Fonseca
 // @match        *://3ds.guide/*
